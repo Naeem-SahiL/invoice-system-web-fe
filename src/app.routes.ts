c78eaf5ada8fa35846a574a1332f3bd0989d8fc9
Naeem SahiL <muhammad.naeem@almusnet.com>
2025-06-25 02:10:22 +0500

    added login page and interceptor

--- src/app.routes.ts
+++ src/app.routes.ts
@@ -4,17 +4,19 @@ import { Dashboard } from './app/pages/dashboard/dashboard';
 import { Documentation } from './app/pages/documentation/documentation';
 import { Landing } from './app/pages/landing/landing';
 import { Notfound } from './app/pages/notfound/notfound';
+import { AuthGuard } from './app/pages/auth/auth.guard';
 
 export const appRoutes: Routes = [
     {
         path: '',
         component: AppLayout,
         children: [
-            { path: '', component: Dashboard },
+            { path: '', component: Dashboard, canActivate: [AuthGuard] },
             { path: 'uikit', loadChildren: () => import('./app/pages/uikit/uikit.routes') },
             { path: 'documentation', component: Documentation },
-            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') }
-        ]
+            { path: 'pages', loadChildren: () => import('./app/pages/pages.routes') , canActivate: [AuthGuard]}
+        ],
+
     },
     { path: 'landing', component: Landing },
     { path: 'notfound', component: Notfound },
